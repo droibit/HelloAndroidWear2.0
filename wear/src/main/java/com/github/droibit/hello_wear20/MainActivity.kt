3979f6e81673561e36a7852fdd9ad5c8e2afbd4c
Shinya Kumgai <roomful.rooms@gmail.com>
2017-10-03 01:37:35 +0900

    Add WearableNavigation/ActionDrawerView

--- wear/src/main/java/com/github/droibit/hello_wear20/MainActivity.kt
+++ wear/src/main/java/com/github/droibit/hello_wear20/MainActivity.kt
@@ -1,15 +1,42 @@
 package com.github.droibit.hello_wear20
 
 import android.os.Bundle
+import android.support.wear.widget.drawer.WearableNavigationDrawerView
 import android.support.wearable.activity.WearableActivity
+import android.view.MenuItem
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : WearableActivity() {
+class MainActivity : WearableActivity(),
+        MenuItem.OnMenuItemClickListener,
+        WearableNavigationDrawerView.OnItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Enables Always-on
-        setAmbientEnabled()
+        topNavigationDrawer.also {
+            it.setAdapter(NavigationAdapter(this))
+            it.addOnItemSelectedListener(this)
+            it.controller.peekDrawer()
+        }
+
+        bottomActionDrawer.also {
+            it.controller.peekDrawer()
+            it.setOnMenuItemClickListener(this)
+        }
+    }
+
+    // WearableNavigationDrawerView.OnItemSelectedListener
+
+    override fun onItemSelected(pos: Int) {
+        Toast.makeText(this, NavigationAdapter.navigationItems[pos].text, Toast.LENGTH_SHORT).show()
+    }
+
+    // MenuItem.OnMenuItemClickListener
+
+    override fun onMenuItemClick(item: MenuItem): Boolean {
+        Toast.makeText(this, item.title, Toast.LENGTH_SHORT).show()
+        return true
     }
 }
---
+++ wear/src/main/java/com/github/droibit/hello_wear20/NavigationAdapter.kt
@@ -0,0 +1,29 @@
+package com.github.droibit.hello_wear20
+
+import android.content.Context
+import android.support.annotation.DrawableRes
+import android.support.v4.content.ContextCompat
+import android.support.wear.widget.drawer.WearableNavigationDrawerView
+
+data class NavigationItem(val text: String, @DrawableRes val iconRes: Int)
+
+class NavigationAdapter(private val context: Context) :
+        WearableNavigationDrawerView.WearableNavigationDrawerAdapter() {
+
+    companion object {
+
+        val navigationItems = listOf(
+                NavigationItem(text = "Android", iconRes = R.drawable.ic_android),
+                NavigationItem(text = "Archive", iconRes = R.drawable.ic_archive),
+                NavigationItem(text = "Build", iconRes = R.drawable.ic_build),
+                NavigationItem(text = "Create", iconRes = R.drawable.ic_create),
+                NavigationItem(text = "AudioTrack", iconRes = R.drawable.ic_audiotrack)
+        )
+    }
+
+    override fun getItemText(pos: Int) = navigationItems[pos].text
+
+    override fun getItemDrawable(pos: Int) = ContextCompat.getDrawable(context, navigationItems[pos].iconRes)!!
+
+    override fun getCount() = navigationItems.size
+}
