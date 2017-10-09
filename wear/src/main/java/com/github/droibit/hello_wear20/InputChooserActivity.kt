ce58dfd8d2ecbbe5beab464a3aa5d0d672e6022c
Shinya Kumgai <roomful.rooms@gmail.com>
2017-10-10 00:41:02 +0900

    WIP: Implement keyboard input

--- wear/src/main/java/com/github/droibit/hello_wear20/InputChooserActivity.kt
+++ wear/src/main/java/com/github/droibit/hello_wear20/InputChooserActivity.kt
@@ -1,18 +1,22 @@
 package com.github.droibit.hello_wear20
 
+import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.speech.RecognizerIntent.*
 import android.support.wearable.activity.WearableActivity
 import android.widget.Toast
+import com.github.droibit.hello_wear20.InputKeyboardActivity.Companion.EXTRA_KEYBOARD_INPUT_RESULT
 import kotlinx.android.synthetic.main.activity_input_chooser.*
 
 class InputChooserActivity : WearableActivity() {
 
     companion object {
 
-        private const val SPEECH_REQUEST_CODE = 0
+        private const val REQUEST_CODE_VOICE_INPUT = 0
+
+        private const val REQUEST_CODE_KEYBOARD_INPUT = 1
 
         @JvmStatic
         fun createIntent(context: Context) = Intent(context, InputChooserActivity::class.java)
@@ -25,22 +29,33 @@ class InputChooserActivity : WearableActivity() {
         inputVoice.setOnClickListener {
             val intent = Intent(ACTION_RECOGNIZE_SPEECH).apply {
                 putExtra(EXTRA_LANGUAGE_MODEL, LANGUAGE_MODEL_FREE_FORM)
-
                 putExtra(EXTRA_PROMPT, "Create")
             }
-            startActivityForResult(intent, SPEECH_REQUEST_CODE)
+            startActivityForResult(intent, REQUEST_CODE_VOICE_INPUT)
+        }
+
+        inputKeyboard.setOnClickListener {
+            val intent = InputKeyboardActivity.createIntent(this)
+            startActivityForResult(intent, REQUEST_CODE_KEYBOARD_INPUT)
         }
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == SPEECH_REQUEST_CODE && resultCode == RESULT_OK
-                && data != null) {
-            val spokenText = data.getStringArrayListExtra(EXTRA_RESULTS)[0]
-            if (spokenText.isEmpty()) {
-                return
-            }
+        super.onActivityResult(requestCode, resultCode, data)
 
-            Toast.makeText(this, spokenText, Toast.LENGTH_SHORT).show()
+        if (resultCode != Activity.RESULT_OK || data == null) {
+            return
         }
+
+        val text =  when (requestCode) {
+            REQUEST_CODE_VOICE_INPUT -> data.getStringArrayListExtra(EXTRA_RESULTS)[0]
+            REQUEST_CODE_KEYBOARD_INPUT -> data.getStringExtra(EXTRA_KEYBOARD_INPUT_RESULT)
+            else -> null
+        }
+
+        if (text.isNullOrEmpty()) {
+            return
+        }
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
---
+++ wear/src/main/java/com/github/droibit/hello_wear20/InputKeyboardActivity.kt
@@ -0,0 +1,52 @@
+package com.github.droibit.hello_wear20
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.support.wearable.activity.WearableActivity
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
+import android.view.inputmethod.InputMethodManager.HIDE_IMPLICIT_ONLY
+import android.view.inputmethod.InputMethodManager.SHOW_FORCED
+import kotlinx.android.synthetic.main.activity_input_keyboard.*
+import kotlinx.android.synthetic.main.activity_input_keyboard.view.*
+
+class InputKeyboardActivity : WearableActivity() {
+
+    companion object {
+
+        const val EXTRA_KEYBOARD_INPUT_RESULT = "EXTRA_KEYBORD_INPUT_RESULT"
+
+        @JvmStatic
+        fun createIntent(context: Context) = Intent(context, InputKeyboardActivity::class.java)
+    }
+
+    private val keyboard by lazy { getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_input_keyboard)
+
+        editText.setOnEditorActionListener { v, actionId, _ ->
+            var handled = false
+            if (actionId == EditorInfo.IME_ACTION_GO) {
+                keyboard.hideSoftInputFromWindow(v.windowToken, 0)
+                val data = Intent().apply { putExtra(EXTRA_KEYBOARD_INPUT_RESULT, v.text.toString()) }
+                setResult(Activity.RESULT_OK, data)
+                finish()
+                handled = true
+            }
+            handled
+        }
+
+        // FIXME: keyboard dose not show when activity start
+        keyboard.toggleSoftInput(SHOW_FORCED, HIDE_IMPLICIT_ONLY)
+//        keyboard.showSoftInput(container, 0)
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean {
+        return super.onKeyDown(keyCode, event)
+    }
+}
